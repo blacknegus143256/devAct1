fc9b0e5d6cdd9277ce19342ca448d6dc4e1bfcb3
blacknegus143256 <patrick17cord@gmail.com>
2025-02-20 09:09:16 +0800

    By Paul Vince Wahing and Joemarie Estalilla

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentsController;
 Route::get('/students', [StudentsController::class, 'index']);
